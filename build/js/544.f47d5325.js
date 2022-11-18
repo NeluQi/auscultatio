2ebb48e273b0c101a057cdb230558d0a3d2046fe
Егор <nelunull@gmail.com>
2022-11-18 11:45:39 +0300

    fix

--- build/js/544.f47d5325.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunkauscultatio"]=self["webpackChunkauscultatio"]||[]).push([[544],{8544:function(e,t,o){o.r(t),o.d(t,{startFocusVisible:function(){return c}});
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const s="ion-focused",n="ion-focusable",r=["Tab","ArrowDown","Space","Escape"," ","Shift","Enter","ArrowLeft","ArrowRight","ArrowUp","Home","End"],c=e=>{let t=[],o=!0;const c=e?e.shadowRoot:document,i=e||document.body,a=e=>{t.forEach((e=>e.classList.remove(s))),e.forEach((e=>e.classList.add(s))),t=e},u=()=>{o=!1,a([])},d=e=>{o=r.includes(e.key),o||a([])},v=e=>{if(o&&void 0!==e.composedPath){const t=e.composedPath().filter((e=>!!e.classList&&e.classList.contains(n)));a(t)}},f=()=>{c.activeElement===i&&a([])};c.addEventListener("keydown",d),c.addEventListener("focusin",v),c.addEventListener("focusout",f),c.addEventListener("touchstart",u),c.addEventListener("mousedown",u);const E=()=>{c.removeEventListener("keydown",d),c.removeEventListener("focusin",v),c.removeEventListener("focusout",f),c.removeEventListener("touchstart",u),c.removeEventListener("mousedown",u)};return{destroy:E,setFocus:a}}}}]);
-//# sourceMappingURL=544.f47d5325.js.map
--- build/js/990.edbce2e6.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunkauscultatio"]=self["webpackChunkauscultatio"]||[]).push([[990],{8990:function(t,e,n){n.r(e),n.d(e,{createSwipeBackGesture:function(){return i}});var r=n(6587),a=n(545),c=n(6515);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const i=(t,e,n,i,o)=>{const s=t.ownerDocument.defaultView,u=(0,a.i)(t),l=t=>{const e=50,{startX:n}=t;return u?n>=s.innerWidth-e:n<=e},h=t=>u?-t.deltaX:t.deltaX,d=t=>u?-t.velocityX:t.velocityX,f=t=>l(t)&&e(),k=t=>{const e=h(t),n=e/s.innerWidth;i(n)},w=t=>{const e=h(t),n=s.innerWidth,a=e/n,c=d(t),i=n/2,u=c>=0&&(c>.2||e>i),l=u?1-a:a,f=l*n;let k=0;if(f>5){const t=f/Math.abs(c);k=Math.min(t,540)}o(u,a<=0?.01:(0,r.h)(0,a,.9999),k)};return(0,c.createGesture)({el:t,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:f,onStart:n,onMove:k,onEnd:w})}}}]);
-//# sourceMappingURL=990.edbce2e6.js.map
