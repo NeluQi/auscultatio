a10295f3f387df3e96255eb1bef46a583ea953f6
Егор <nelunull@gmail.com>
2022-11-18 11:46:06 +0300

    added build

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appName: 'auscultatio',
-  webDir: 'dist',
-  bundledWebRuntime: false
-};
-
-export default config;
